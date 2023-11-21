60efa0f459c18916b91c140aa720aa53e887e776
Yu Mei <916650903@qq.com>
2023-11-21 18:33:18 +0800

    left bar

--- ttd/user/i/App.vue
+++ ttd/user/i/App.vue
@@ -4,24 +4,33 @@
 
 <template>
   <div class="main flex flex-row grow h-full w-full">
-    <div class="serverlist flex bg-gray-950"></div>
+    <div class="serverlist flex"></div>
     <div class="server flex flex-row w-full">
-      <div class="toollist flex flex-auto bg-gray-800"></div>
-      <div class="tooldetail flex grow basis-full bg-gray-400 w-full"></div>
+      <div class="toollist flex flex-auto bg-orange-100"></div>
+      <div class="tooldetail flex grow basis-full bg-orange-50 w-full"></div>
     </div>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .main {
   height: 100%;
-}
-.serverlist {
-  width: 72px;
+  .serverlist {
+    background-color: #1e1f22;
+    width: 72px;
+  }
+
+  .server {
+    .toollist {
+      background-color: #2b2d31;
+      width: 240px;
+    }
+
+    .tooldetail {
+      background-color: #313338;
+    }
+  }
 }
 
-.toollist {
-  width: 240px;
-}
 
 </style>
