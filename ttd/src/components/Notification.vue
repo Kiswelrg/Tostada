95ed789121311fe1b14327e333cfa9270dfc52a7
Yu Mei <916650903@qq.com>
2023-12-06 18:10:45 +0800

    Create Notification.vue

---
+++ ttd/src/components/Notification.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        {{ notification }}
+    </div>
+</template>
+
+<script setup>
+const p = defineProps(['notification'])
+const notification = p.notification
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
