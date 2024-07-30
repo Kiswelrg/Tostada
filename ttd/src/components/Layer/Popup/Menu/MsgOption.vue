7bb4445ff47c70aebf43ce62d22b94e83f56cd21
Yu Mei <916650903@qq.com>
2024-07-30 23:21:04 +0800

    Update MsgOption.vue

--- ttd/src/components/Layer/Popup/Menu/MsgOption.vue
+++ ttd/src/components/Layer/Popup/Menu/MsgOption.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="menu relative flex min-w-[188px] max-w-[320px] h-auto cursor-default max-h-[calc(100vh-32px)] rounded-[4px] bg-[var(--background-floating)] shadow-[var(--shadow-high)] z-10">
-        <div class="relative text-white scroller px-2 py-1.5 ">
+        <div class="relative text-white scroller px-2 py-1.5 flex-auto">
             <div class="group pt-1 pr-0 pb-1.5 pl-0.5 h-8">1</div>
             <div class="group">
-                <item class="">Edit Message</item>
-                <item class="">Copy Text</item>
-                <item class="">Copy Message Link</item>
-                <item class="">Delete Message</item>
+                <item class="flex min-h-8 items-center px-2 py-1.5 justify-between mx-0 my-0.5 rounded-[2px] text-[14px] font-medium leading-[18px] cursor-pointer outline-0">Edit Message</item>
+                <item class="flex min-h-8 items-center px-2 py-1.5 justify-between mx-0 my-0.5 rounded-[2px] text-[14px] font-medium leading-[18px] cursor-pointer outline-0">Copy Text</item>
+                <item class="flex min-h-8 items-center px-2 py-1.5 justify-between mx-0 my-0.5 rounded-[2px] text-[14px] font-medium leading-[18px] cursor-pointer outline-0">Copy Message Link</item>
+                <item class="flex min-h-8 items-center px-2 py-1.5 justify-between mx-0 my-0.5 rounded-[2px] text-[14px] font-medium leading-[18px] cursor-pointer outline-0">Delete Message</item>
             </div>
         </div>
     </div>
